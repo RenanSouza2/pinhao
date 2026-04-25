ee7475e2be72d46db6e6bf88f623a87702a362df
RenanSouza2 <renan.rodrigues.souza1@gmail.com>
2026-04-25 12:34:18 -0300

    aaa

--- src/main.c
+++ src/main.c
@@ -7,24 +7,9 @@
 #include "../mods/number/lib/num/struct.h"
 
 #include "../lib/big/header.h"
-#include "../lib/split/header.h"
-#include "../lib/linear/header.h"
-#include "../lib/union/header.h"
+// #include "../lib/linear/linear/header.h"
 
 
-
-void time_1(void)
-{
-    for(uint64_t i=1000; i <= 65000; i+=1000)
-    {
-        printf("\n" U64P() "", i);
-        TIME_SETUP
-        fxd_num_t flt = pi_v1(i);
-        TIME_END(t3)
-        printf("\t%.2f", (double)t3 / 1e9);
-        fxd_num_free(flt);
-    }
-}
 
 void pi(uint64_t size)
 {
@@ -37,26 +22,39 @@ void pi(uint64_t size)
 int main(void)
 {
     setbuf(stdout, NULL);
-    
-    // use();
+    printf("\nbegin");
 
-    uint64_t size = 100 * 1000;
-    pi(size);
+    // clu_log_level_set(CLU_LOG_DYNAMIC);
+
+    pi(1024 * 1024);
 
     // prepare(16, 48000, 48829, 6);
     // prepare(17, 24408, 24414, 6);
     // prepare(18, 12204, 12207, 6);
     // prepare(19, 6102, 6103, 6);
-    // prepare(20, 3000, 3051, 6);
-    // prepare(21, 1520, 1525, 6);
-    // prepare(22, 760, 762, 6);
-    // prepare(23, 380, 381, 6);
-    // prepare(24, 230, 1907, 12);
+    // prepare(20, 0, 30, 6);
+    // prepare(21, 2, 15, 6);
+    // prepare(22, 0, 76, 6);
+    // prepare(23, 4, 38, 6);
+    // prepare(24, 0, 19, 6);
     // prepare(25, 94, 95, 6);
     // prepare(26, 60, 476, 12);
     // prepare(27, 24, 238, 1);
     // prepare(28, 1, 119, 12);
 
+    // printf("\n\nmax_occupancy: : " U64P() "", clu_get_max_occupancy());
+    // assert(clu_mem_is_empty());
+
+    // flt_num_t flt_pi = pi_v3(1024 * 1024);
+    // flt_num_display_dec(flt_pi);
+    // flt_num_free(flt_pi);
+
     printf("\n");
     return 0;
 }
+
+// gcc 13
+
+// v10, new lib: 34m32.111s
+// v9, old lib: 35m42.555s
+// v9: new lib: 32m55.631s
