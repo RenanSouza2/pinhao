abcfd018e2328c3cc0f8797c18690246496b9ca1
RenanSouza2 <renan.rodrigues.souza1@gmail.com>
2026-07-06 20:37:30 -0300

    update araucaria

--- makefiles/flags.mk
+++ makefiles/flags.mk
@@ -1,6 +1,6 @@
 FLAGS = -std=c23 -Wall -Wextra -Wpedantic -Werror -Wfatal-errors -Wshadow -Wpointer-arith -Wcast-qual -Wwrite-strings -Wundef -Wformat=2 -Wformat-signedness -Wnull-dereference -Wconversion -Wsign-conversion -D_POSIX_C_SOURCE=200809L -Wimplicit-fallthrough -Wfloat-equal -Wredundant-decls -Wdouble-promotion -Wmissing-include-dirs -Wswitch-enum -Wnested-externs -Wmissing-prototypes -Wdate-time -Wmissing-declarations -Walloca -Wvla
 
-FLAGS_PRD = -O2 -march=native -fstack-protector-strong -D_FORTIFY_SOURCE=3 -ffunction-sections -fdata-sections -flto=auto
+FLAGS_PRD = -O3 -march=native -ffunction-sections -fdata-sections -flto=auto -g
 FLAGS_DBG = -D DEBUG -O0 -g3 -ggdb -fno-omit-frame-pointer -fsanitize=address,undefined -fno-optimize-sibling-calls
 
 FLAGS_CMP = -c
@@ -8,7 +8,7 @@ FLAGS_LNK = -r -nostdlib
 FLAGS_EXE =
 
 ifeq ($(shell uname -s),Linux)
-	FLAGS += -fanalyzer -Wduplicated-cond -Wduplicated-branches -Wlogical-op -Wcast-align=strict -Walloc-zero -Wtrailing-whitespace -Wleading-whitespace=spaces
+	FLAGS += -Wduplicated-cond -Wduplicated-branches -Wlogical-op -Wcast-align=strict -Walloc-zero -Wtrailing-whitespace -Wleading-whitespace=spaces
 
     FLAGS_PRD += -fstack-clash-protection -fcf-protection=full
 	FLAGS_DBG += -fsanitize=leak
